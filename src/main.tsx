7b155c35fab7633600312f66e0ca60a784343bfe
unknown <muryllo.henrique@hotmail.com>
2022-12-12 18:10:06 -0300

    Starting to idealize the project style

--- src/main.tsx
+++ src/main.tsx
@@ -6,5 +6,5 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Home />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
