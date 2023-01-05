b0d3c70b9c33491c9421efdc7bf43e880536dbe8
unknown <muryllo.henrique@hotmail.com>
2023-01-05 12:58:40 -0300

    Finishing the skills section responsivity

--- src/components/Skills/Skill.tsx
+++ src/components/Skills/Skill.tsx
@@ -1,19 +1,17 @@
-import React from 'react'
-
 type Props = {
-  directionLeft?: boolean;
+
 }
 
-function Skill({ directionLeft }: Props) {
+function Skill({ }: Props) {
   return (
     <div className='group relative flex cursor-pointer'>
       <img
         src="https://camo.githubusercontent.com/5734d0669fe22ce04a1cb989a156cd32c379875f6bca56d5210c9432824856d9/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f7461696c77696e646373732f7461696c77696e646373732d69636f6e2e737667"
-        className='rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out'
+        className='rounded-full border border-gray-500 object-cover w-20 h-20 max-[320px]:w-16 max-[320px]:h-16 filter group-hover:grayscale transition duration-300 ease-in-out'
       />
-      <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0">
+      <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-20 w-20 max-[320px]:w-16 max-[320px]:h-16 rounded-full z-0">
         <div className="flex items-center justify-center h-full">
-          <p className='text-3xl font-bold text-black opacity-100'>
+          <p className='text-2xl font-bold text-black opacity-100'>
             100%
           </p>
         </div>
--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -8,25 +8,25 @@ function Skills({ }: Props) {
     return (
         <div 
         className='flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center'>
-            <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Skills</h3>
+            <h3 className='absolute top-20 uppercase tracking-[20px] text-gray-500 text-2xl'>Skills</h3>
 
-            <h3 className='absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm'>
+            <h3 className='absolute top-32 uppercase tracking-[3px] text-gray-500 text-sm'>
                 Hover over a skill for current proficiency
             </h3>
 
-            <div className='grid grid-cols-4 gap-5'>
+            <div className='grid grid-cols-3 gap-5 mt-28 max-[320px]:grid-cols-4 max-[320px]:mt-36'>
                 <Skill />
                 <Skill />
                 <Skill />
                 <Skill />
                 <Skill />
                 <Skill />
-                <Skill directionLeft />
-                <Skill directionLeft />
-                <Skill directionLeft />
-                <Skill directionLeft />
-                <Skill directionLeft />
-                <Skill directionLeft />
+                <Skill />
+                <Skill />
+                <Skill />
+                <Skill />
+                <Skill />
+                <Skill />
             </div>
         </div>
     )
