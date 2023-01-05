7b5b73dad1cc37e3d8110030f70bff585594be37
unknown <muryllo.henrique@hotmail.com>
2023-01-05 12:38:30 -0300

    Finishing the experience section responsivity

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { motion } from 'framer-motion'
 import ExperienceCard from './ExperienceCard'
 
@@ -11,7 +10,7 @@ function Experience({ }: Props) {
             whileInView={{ opacity: 1 }}
             transition={{ duration: 1.5 }}
             className='h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'>
-            <h3 className='absolute top-20 uppercase tracking-[20px] text-gray-500 text-2xl'>
+            <h3 className='absolute top-20 uppercase tracking-[20px] text-gray-500 text-2xl max-[320px]:text-xl'>
                 Experience
             </h3>
 
--- src/components/Experience/ExperienceCard.tsx
+++ src/components/Experience/ExperienceCard.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
 import { motion } from 'framer-motion'
 
 type Props = {}
 
 function ExperienceCard({}: Props) {
   return (
-    <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] h-[570px] mt-20 md:w-[600px] xl:w-[700px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden'>
+    <article className='flex flex-col rounded-lg items-center space-y-5 flex-shrink-0 w-[500px] h-[450px] mt-20 max-[320px]:h-[330px] md:w-[600px] md:h-[600px] xl:w-[700px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden'>
         <motion.img 
         initial={{
             y: -100,
@@ -14,24 +13,20 @@ function ExperienceCard({}: Props) {
         transition={{ duration: 1.2 }}
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true }}
-        className='w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center' src="https://avatars.githubusercontent.com/u/105292489?v=4" 
+        className='w-28 h-28 rounded-full max-[320px]:-mt-5 xl:w-[150px] xl:h-[150px] object-cover object-center' src="https://avatars.githubusercontent.com/u/105292489?v=4" 
         alt="" />
 
         <div className='px-0 md:px-10'>
-            <h4 className='text-4xl font-light'>Freelancer</h4>
-            <p className='font-bold text-2xl mt-1'>Myself</p>
+            <h4 className='text-3xl font-light'>Freelancer</h4>
+            <p className='font-bold text-xl'>Myself</p>
             <div className='flex space-x-2 my-2'>
                 <img className='h-10 w-10 rounded-full' src="https://camo.githubusercontent.com/7a2b6137fa6818b1c85f86347a6b4a75ee52681d4a190c506df972e3c5459980/68747470733a2f2f70726f66696c696e61746f722e7269736861762e6465762f736b696c6c732d6173736574732f6a6176617363726970742d6f726967696e616c2e737667" alt="" />
                 <img className='h-10 w-10 rounded-full' src="https://camo.githubusercontent.com/7a2b6137fa6818b1c85f86347a6b4a75ee52681d4a190c506df972e3c5459980/68747470733a2f2f70726f66696c696e61746f722e7269736861762e6465762f736b696c6c732d6173736574732f6a6176617363726970742d6f726967696e616c2e737667" alt="" />
                 <img className='h-10 w-10 rounded-full' src="https://camo.githubusercontent.com/7a2b6137fa6818b1c85f86347a6b4a75ee52681d4a190c506df972e3c5459980/68747470733a2f2f70726f66696c696e61746f722e7269736861762e6465762f736b696c6c732d6173736574732f6a6176617363726970742d6f726967696e616c2e737667" alt="" />
             </div>
-            <p className='uppercase py-5 text-gray-300'>Started work... - Ended...</p>
+            <p className='uppercase py-2 text-gray-300'>Started work... - Ended...</p>
 
-            <ul className='list-disc space-y-4 ml-5 text-lg max-h-96 overflow-y-scroll pr-5 scrollbar-thin scrollbar-track-black scrollbar-thumb-[#F7AB0A]/80'>
-                <li>Summary points</li>
-                <li>Summary points</li>
-                <li>Summary points</li>
-                <li>Summary points</li>
+            <ul className='list-disc space-y-2 ml-5 text-lg max-h-96 overflow-y-scroll pr-5 max-[428px]:hidden scrollbar-thin scrollbar-track-black scrollbar-thumb-[#F7AB0A]/80'>
                 <li>Summary points</li>
                 <li>Summary points</li>
                 <li>Summary points</li>
